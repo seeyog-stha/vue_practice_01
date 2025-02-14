09ea34f80c1a04f971470ee03497877d1f635396
seeyog-stha <jabeens.calcgen@gmail.com>
2025-02-14 20:55:32 +0545

    feat: template and reactive

---
+++ src/views/ReactiveView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref,nextTick ,reactive} from 'vue';
+const values = ref(0);
+const obj = {id:ref(0)}
+let tests = 0
+// limitation of reactive 
+// first is not applicable to basic types like string Number
+// destructuring causes it to loose its connection 
+// cannot replace the entire obj 
+const react = reactive({count:0})
+const increments = async() => {
+    values.value++;
+    await nextTick()
+    values.value++;
+    values.value++;
+}
+const testincrease = () => {
+    tests++;
+
+}
+const reactincrease =()=>{
+    react.count++
+}
+const {id} = obj
+</script>
+<template>
+    <main>
+
+        <button @click="increments">inc</button>
+        <button @click="testincrease">inc test</button>
+        <button @click="reactincrease">inc react</button>
+        <p>{{ values }}</p>
+        <p>test:{{ tests }}</p>
+        <p>test:{{ react.count}}</p>
+        <p>test:{{ obj.id}}</p>
+        <p>test:{{ obj.id+1}}</p>
+        <p>test:{{ id+1}}</p>
+    </main>
+</template>
---
+++ src/views/TemplateView.vue
@@ -0,0 +1,29 @@
+<script setup>
+const msg = "hello world"
+const html = "<span>hello form html</span>"
+const id = 2
+let isdisabled = true // "" is also treated as true
+//   incase of multiple attributes
+const objectOfAttrs = {
+    id: 'container',
+    class: 'wrapper',
+    style: 'background-color:green'
+}
+const red = () => {
+    console.log("hello")
+}
+const event= "click"
+</script>
+<template>
+    <p>{{ msg }}</p>
+    <p>{{ html }}</p>
+    <p v-html="html"></p>
+    <p :id="`id-${12}`">elite</p>
+    <button :disabled="isdisabled">elite</button>
+    <p v-bind="objectOfAttrs">these comp has multiple attributes</p>
+    <p>{{ red() }}</p>
+    <p v-if="isdisabled"> this is v-if</p>
+    <button @click="red">click me</button>
+    <button @[event]="red">same click me</button>
+    <form @submit.prevent="red"></form>
+</template>
